645e87edf63aa8d5adcf7efc7184fa7c5ec4bfa8
HuangCongQing <1756260160@qq.com>
2020-07-20 15:39:41 +0000

    update

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, 'models'))
 sys.path.append(os.path.join(BASE_DIR, 'utils'))
+print(sys.path)
 import provider
 import tf_util
 
